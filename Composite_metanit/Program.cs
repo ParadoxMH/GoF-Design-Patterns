5ef911c97467e8aa0185eeee7b950366d6407e96
mhorditsa <mhorditsa@homeaway.com>
2019-09-04 11:28:59 +0300

    add Adapter add Composite

---
+++ Composite_metanit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Composite_metanit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
